bc896161736963892d7c4a9a1a73d71b193d76b3
Frank <frankmarsman@gmail.com>
2018-03-10 16:09:59 +0100

    Added memory game
    Now you can practice faces

--- jscode.js
+++ jscode.js
@@ -1,39 +1,87 @@
 
 // Get information about user:
-var __url = "https://api.ciscospark.com/v1/people/me";
 
 var CNT = "application/json; charset=utf-8";
 var TKN = "M2U1MDIyMzEtNDNlMS00Y2Y2LWIxNDItN2MwNDdhOGM0NjRmNTE5Zjk5NmMtYTA1";
 var AUT = "Bearer " + TKN;
 
 var PERSONS = [] // array of all participants
+var UNKNOWNS = []; // list of unknown people (indices)
+var CUR_UNKNOWN = 0;
 
-function ShowResult(result) {
-  document.getElementById("hans").innerHTML = JSON.stringify(JSON.parse(JSON.stringify(result)),null,2); ;
-  //lert(JSON.stringify(result) + "LOLOLOL: " + result.avatar);
-  document.getElementById("imgShow").src = result.avatar;
-} // ShowResult
+function DispNameLeft(faceNr) {
+  if (faceNr >= 0 && faceNr < PERSONS.length) {
+    document.getElementById("faceNameHolder").innerHTML = PERSONS[faceNr].name;
+  } // if
+  else {
+    document.getElementById("faceNameHolder").innerHTML = "no selection";
+  } // else
+} // DispNameLeft
 
+// displays face [UNKNOWNS[n]]
+function SetRFace(n) {
+  if (UNKNOWNS.length == 0) {
+    jQuery("#rfaceimage").attr('src', "defaultface.png");
+    document.getElementById("rfacetext").innerHTML = "nobody";
+  } // if
+  else {
+    var faceNr = UNKNOWNS[n];
+    jQuery("#rfaceimage").attr('src', PERSONS[faceNr].imgURL);
+    document.getElementById("rfacetext").innerHTML = PERSONS[faceNr].name;
+  } // else
+  var str = "";
+  str += PERSONS.length + " faces, " + UNKNOWNS.length + " unknown";
+  document.getElementById("rinfo").innerHTML = str;
+} // SetRFace
 
-jQuery.ajax({
-  url: __url,
-  type: "GET",
-  headers: {
-    'Authorization': AUT,
-    'Content-Type': CNT
-  }, // headers
-  success: function(result) {
-    ShowResult(result);
-  } // function
-}); // jQuery.ajax
+// chooses random next index for face
+function NextRFace( ) {
+  var temp = CUR_UNKNOWN;
+  if (UNKNOWNS.length <= 1) {
+    console.log("UNKNOWNS.length == " + UNKNOWNS.length + "!");
+    CUR_UNKNOWN = 0;
+    SetRFace(CUR_UNKNOWN);
+    return;
+  } // if
+  while (CUR_UNKNOWN == temp) {
+    CUR_UNKNOWN = Math.floor(Math.random( ) * UNKNOWNS.length);
+    console.log("CUR_UNKNOWN = " + CUR_UNKNOWN + " temp = " + temp);
+  } // while
+  SetRFace(CUR_UNKNOWN);
+} // NextRFace
+
+// removes element [CUR_UNKNOWN] from array UNKNOWNS
+function RemoveCurrentRFace( ) {
+  if (UNKNOWNS.length <= 1) {
+    UNKNOWNS = [ ];
+    CUR_UNKNOWN = 0;
+    NextRFace( );
+    return;
+  } // if
+  var newArray = [ ];
+  for (var x = 0; x < UNKNOWNS.length; x++) {
+    if (x != CUR_UNKNOWN) {
+      newArray[newArray.length] = UNKNOWNS[x];
+    } // if
+  } // for
+  UNKNOWNS = newArray;
+  CUR_UNKNOWN = 0;
+  NextRFace( );
+} // RemoveCurrentRFace
 
 // displays all elements in array PERSONS
 function ShowPersonList( ) {
-  document.getElementById("hans").innerHTML = "";
+  document.getElementById("facecontainer").innerHTML = "";
   for (var i = 0; i < PERSONS.length; i++) {
-    var str = "<img width='128' height='128' src = '" + PERSONS[i].imgURL + "'>";
-    str += i + ": " + PERSONS[i].name + "<br/><br/>";
-    document.getElementById("hans").innerHTML += str;
+    var str = "";
+    str += "<div class='pcolumn'>";
+    str += "<img style='width:100%' class='faceimg' ";
+    str += "id='person_face_" + i + "' "; // set id to person_face_[i]
+    str += "src='" + PERSONS[i].imgURL + "' "; // add image
+    str += "onmouseover='DispNameLeft(" + i + ");'";
+    str += "></div>";
+
+    document.getElementById("facecontainer").innerHTML += str;
   } // for
 } // ShowPersonList
 
@@ -51,13 +99,23 @@ function ProcessParticipants(result) {
       headers: _header,
       success: function(result) {
         if (result.type == "person") {
-          var person = new Object;
-          person.name = result.displayName;
-          person.imgURL = result.avatar;
-          person.id = result.id;
+          if (result.hasOwnProperty('avatar')) {
+            var person = new Object;
+            person.name = result.displayName;
+            person.imgURL = result.avatar;
+            person.id = result.id;
 
-          PERSONS[PERSONS.length] = person;
-          ShowPersonList( );
+            PERSONS[PERSONS.length] = person;
+
+            // update UNKNOWNS:
+            UNKNOWNS = [];
+            for (var n = 0; n < PERSONS.length; n++)
+              UNKNOWNS[n] = n;
+
+            SetRFace(0);
+
+            ShowPersonList( );
+          } // if
         } // if
       } // function
     }); // jQuery.ajax
@@ -80,10 +138,11 @@ function GetRoomParticipants( ) {
     headers: _header,
     data: _params,
     success: function(result) {
-      //alert(JSON.stringify(result));
       ProcessParticipants(result);
     } // function
   }); // jQuery.ajax
 } // GetRoomParticipants
 
 document.getElementById("roomBut").onclick = GetRoomParticipants;
+document.getElementById("nextfacebutton").onclick = NextRFace;
+document.getElementById("removefacebutton").onclick = RemoveCurrentRFace;
